ee049a6688a91b641a96f0d42f17a65918c9e0ae
Rocket ! <rocketnova@users.noreply.github.com>
2023-08-22 20:55:47 -0700

    WIP migrate functionality.

--- app/anki_chinese/test.py
+++ app/anki_chinese/test.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 import click
-import yaml
+from anki_chinese.utils import *
+import logging
 
 @click.command()
-@click.argument('vocab_file', type=click.File('rb'))
-@click.option('-c', '--char-file', type=click.File('wb'), help='A text file containing a character list (prevents duplicate cards in Anki).')
+@click.argument('vocab_file')
+@click.option('-c', '--char-file', help='A text file containing a character list (prevents duplicate cards in Anki).')
 @click.option('--no-update-char', is_flag=True, help="Must be used in conjuction with -c/--char-file. Will use an existing character list, but won't update it.")
 @click.option('-t', '--tags', multiple=True, help="Tags to be applied to every entry in the CSV. Multiple tags must be double quoted.")
-@click.option('-v', '--verbose', help="Increase output verbosity.")
+@click.option('-v', '--verbose', is_flag=True, help="Increase output verbosity.")
 def main(vocab_file, char_file, no_update_char, tags, verbose):
     """
     Generates a CSV of Hanzi (Chinese vocabulary), English meaning, zhuyin, pinyin, and Taiwan stroke order for import into Anki.
@@ -17,24 +18,26 @@ def main(vocab_file, char_file, no_update_char, tags, verbose):
 
     VOCAB_FILE should be in the format: `十二月底: The end of December`
     """
+    # Setup logging
+    if verbose:
+      loglevel = logging.DEBUG
+    else:
+      loglevel = logging.INFO
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
 
     to_csv = []
 
     click.echo("Hello World")
-    for tag in tags:
-        click.echo(tag)
 
+    # Read files into variables.
+    vocab = load_vocab(vocab_file)
+    if char_file:
+        char_list = load_char_list(char_file)
+    else:
+        char_list = ""
 
-# Parses yaml containing vocabulary.
-# Expects the following format
-#   PHRASE: MEANING
-# Example:
-#   臺灣: Taiwan
-#   接: To pick someone up
-def load_vocab(vocab_file):
-    vocab = ''
-    with open(vocab_file) as vf:
-        vocab = yaml.safe_load(vf)
+    click.echo(char_list)
+
 
 if __name__ == "__main__":
     main()
