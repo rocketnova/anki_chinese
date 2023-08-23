596a375497fc382f6906fcfe0e8e39a249c4ec86
Rocket ! <rocketnova@users.noreply.github.com>
2023-08-22 23:06:41 -0700

    Tidy app dir.

--- app/anki_chinese/csv.py
+++ app/anki_chinese/csv.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
-# Template based on https://gist.github.com/opie4624/3896526
 
-# Import template modules
-import argparse, logging
-from ac_util import *
+import click
+from anki_chinese.utils import *
+import logging
 
-# Generates a CSV of Chinese vocabulary with accompanying zhuyin images and Taiwan stroke order for import into Anki.
-# - CSV: anki-import.csv (will append tags as the last field)
-# - SVGs: 學.svg
-# - PNGs: 學.png
-# - Stroke PNGs: 學-stroke.png
-def main(args, loglevel):
+@click.command()
+@click.argument('vocab_file')
+@click.option('-c', '--char-file', help='A text file containing a character list (prevents duplicate cards in Anki).')
+@click.option('--no-update-chars', is_flag=True, help="Must be used in conjuction with -c/--char-file. Will use an existing character list, but won't update it.")
+@click.option('--no-update-images', is_flag=True, help="Skip updating existing images.")
+@click.option('-t', '--tags', multiple=True, help="Tags to be applied to every entry in the CSV. Multiple tags must be double quoted.")
+@click.option('-v', '--verbose', is_flag=True, help="Increase output verbosity.")
+def main(vocab_file, char_file, no_update_chars, no_update_images, tags, verbose):
+    """
+    Generates a CSV of Chinese vocabulary, English meaning, zhuyin, pinyin, and Taiwan stroke order for import into Anki.
+
+    VOCAB_FILE is a vocab list in YAML format.
+
+    VOCAB_FILE should be in the format: `十二月底: The end of December`
+    """
+    # Setup logging.
+    if verbose:
+      loglevel = logging.DEBUG
+    else:
+      loglevel = logging.INFO
     logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
-
-    vocab_file = args.vocab_file
-    char_file = args.char_file
-    no_update_char = args.no_update_char
-    update = args.update
-    tags = args.tags
-    to_csv = []
 
     # Read files into variables.
     vocab = load_vocab(vocab_file)
@@ -28,106 +34,72 @@ def main(args, loglevel):
         char_list = ""
 
     # Prepare image directors.
-    make_image_dirs()
+    stroke_dir = "images/stroke_order"
+    make_image_dirs(["images", stroke_dir])
 
     # For each new vocab word, create a new CSV row entry that contains:
     # - Chinese phrase
     # - English meaning
-    # - Image tag of zhuyin for the phrase
+    # - Zhuyin for the phrase
+    # - Pinyin for the phrase
     # - Image tag(s) for the stroke order for the characters in the phrase
     # - For each unique character in the phrase:
-    #   - Chinese character
-    #   - Image tag of zhuyin
+    #   - Hanzi
+    #   - Zhuyin
+    #   - Pinyin
+    rows = []
+    longest_phrase_count = 0
     for phrase, meaning in vocab.items():
+        # Append the phrase and meaning to the row.
         row = [phrase, meaning]
 
-        gen_svg(phrase, update)
-        gen_png(phrase, update)
-        row.append(char_tag(phrase))
+        # Add zhuyin and pinyin to the row.
+        row.append(get_zhuyin(phrase))
+        row.append(get_pinyin(phrase))
 
-        stroke_tags = gen_stroke(phrase, update)
-        # DEBUG START
-        # stroke_tages = ""
-        # for char in phrase:
-        #     stroke_tags = stroke_tags + stroke_tag(char)
-        # DEBUG END
+        # Add stroke order to the row.
+        stroke_tags = gen_stroke(phrase, stroke_dir, no_update_images)
         row.append(stroke_tags)
 
+        # Process each character in the Chinese phrase.
         for char in phrase:
-            # Update the running list of known characters if it is not already known
+            # If the character is not in the char_list, add it.
             if is_new_char(char_list, char) == True:
                 char_list = char_list + char
-                # Append the character and zhuyin for each character in the phrase.
+                # Add the character, zhuyin, and pinyin to the row.
                 # Skip phrases that are only 1 character long since it would just be a duplicate.
                 if len(phrase) > 1:
-                    gen_svg(char, update)
-                    gen_png(char, update)
                     row.append(char)
-                    row.append(char_tag(char))
-        to_csv.append(row)
-        logging.debug(f"Appending: {row}")
+                    row.append(get_zhuyin(char))
+                    row.append(get_pinyin(char))
 
-    max_length = len(max(to_csv, key=len))
-    # Subtract # non character fields (Chinese phrase, English meaning, Phrase zhuyin, Phrase strokes)
-    # Don't include tags, which is added later
-    max_char = (max_length - 4) / 2
+        # Keep track of the longest vocab phrase.
+        # This gets output as a warning when the csv is created.
+        if len(phrase) > longest_phrase_count:
+            longest_phrase_count = len(phrase)
+
+        # Save the row.
+        rows.append(row)
+        logging.debug(f"Adding row: {row}")
 
     # Make all rows the same length.
     # Add tags as the last field.
-    for row in to_csv:
-        row[:] = row + [''] * (max_length - len(row)) + [tags]
+    max_length = len(max(rows, key=len))
+    for row in rows:
+        row[:] = row + [''] * (max_length - len(row)) + [' '.join(tags)]
 
     # Update the character file.
-    if char_file and no_update_char == False:
+    if char_file and no_update_chars == False:
         write_text(char_file, char_list)
 
     # Write the CSV.
-    write_csv(to_csv)
+    write_csv(rows, "import_into_anki.csv")
 
     # Print summary.
-    logging.info(f"Generated CSV for {len(to_csv)} characters.")
-    if (max_char > 0):
-        logging.warning(f"Anki must be able to support at least {int(max_char)} character fields.")
+    logging.info(f"Generated CSV for {len(rows)} vocabulary phrases.")
+    if (longest_phrase_count > 0):
+        logging.warning(f"Anki note type must be able to support at least {int(longest_phrase_count)} character fields.")
 
 
-# Standard boilerplate to call the main() function to begin the program.
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-                                      description = "Generates a CSV of Chinese vocabulary with accompanying zhuyin images and Taiwan stroke order for import into Anki.",
-                                      epilog = "As an alternative to the commandline, params can be placed in a file, one per line, and specified on the commandline like '%(prog)s @params.conf'.",
-                                      fromfile_prefix_chars = '@' )
-    parser.add_argument(
-                        "vocab_file",
-                        help = "A vocab list in YAML format")
-    parser.add_argument(
-                        "-c",
-                        "--char-file",
-                        help = "A text file containing a character list (prevents duplicate cards in Anki)")
-    parser.add_argument(
-                        "--no-update-char",
-                        help="Must be used in conjuction with -c/--char-file. Will read in an existing character file, but won't update it",
-                        action="store_true")
-    parser.add_argument(
-                        "-t",
-                        "--tags",
-                        help="Tags to be applied every entry in the CSV. Multiple tags must be double quoted",
-                        metavar = "TAGS")
-    parser.add_argument(
-                        "-u",
-                        "--update",
-                        help="Update (overwrite) any existing images",
-                        action="store_true")
-    parser.add_argument(
-                        "-v",
-                        "--verbose",
-                        help="Increase output verbosity",
-                        action="store_true")
-    args = parser.parse_args()
-
-    # Setup logging
-    if args.verbose:
-      loglevel = logging.DEBUG
-    else:
-      loglevel = logging.INFO
-
-    main(args, loglevel)
+if __name__ == "__main__":
+    main()
